2cc439f0cc16d0f5cdd06d57e8b24d43365f69e4
D1ABL05 <CFairchild1207@gmail.com>
2017-06-03 23:24:06 -0500

    attempting to add form switching

---
+++ resources/js/formsel.js
@@ -0,0 +1,25 @@
+$("#formulario1").click(function() {
+  document.getElementById("form1").reset();
+  document.getElementById("form2").reset();
+  document.getElementById("form3").reset();
+  $("#form1").toggle();
+  $("#form2").hide();
+  $("#form3").hide();
+});
+
+$("#formulario2").click(function() {
+  document.getElementById("form1").reset();
+  document.getElementById("form2").reset();
+  document.getElementById("form3").reset();
+  $("#form2").toggle();
+  $("#form1").hide();
+  $("#form3").hide();
+});
+$("#formulario3").click(function() {
+  document.getElementById("form1").reset();
+  document.getElementById("form2").reset();
+  document.getElementById("form3").reset();
+  $("#form3").toggle();
+  $("#form2").hide();
+  $("#form1").hide();
+});
