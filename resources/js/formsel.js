594f9e11b7103a3fcbb39952c6ae605d18e70e9c
D1ABL05 <CFairchild1207@gmail.com>
2017-06-04 10:46:34 -0500

    removed reset.css,  might be clashing with form

--- resources/js/formsel.js
+++ resources/js/formsel.js
@@ -2,7 +2,7 @@ $("#formulario1").click(function() {
   document.getElementById("form1").reset();
   document.getElementById("form2").reset();
   document.getElementById("form3").reset();
-  $("#form1").toggle();
+  $("#form1").show();
   $("#form2").hide();
   $("#form3").hide();
 });
@@ -11,15 +11,16 @@ $("#formulario2").click(function() {
   document.getElementById("form1").reset();
   document.getElementById("form2").reset();
   document.getElementById("form3").reset();
-  $("#form2").toggle();
+  $("#form2").show();
   $("#form1").hide();
   $("#form3").hide();
 });
+
 $("#formulario3").click(function() {
   document.getElementById("form1").reset();
   document.getElementById("form2").reset();
   document.getElementById("form3").reset();
-  $("#form3").toggle();
+  $("#form3").show();
   $("#form2").hide();
   $("#form1").hide();
 });
